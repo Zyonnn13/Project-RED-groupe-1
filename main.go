9e14e0ec604c17bb24a7fb195b91d4c347c239a1
Mathys.E <mathys.eustache@ynov.com>
2025-09-15 11:59:17 +0200

    ajout.go

--- main.go
+++ main.go
@@ -84,23 +84,6 @@ ________/\\\\\\\\\________________/\\\__________________________________________
 	fmt.Println("\nTu entres dans le shop...")
 	pv.AfficherPV(etat)
 
-	// Début du combat
-	etat = player.EtatCombat
-	fmt.Println("\nUn combat commence !")
-	pv.SubirDegats(30)
-	pv.AfficherPV(etat)
-
-	// Soin dans le shop
-	etat = player.EtatShop
-	fmt.Println("\nTu te soignes dans le shop...")
-	pv.Soigner(20)
-	pv.AfficherPV(etat)
-
-	if pv.EstEnVie() {
-		fmt.Println("Tu es toujours en vie.")
-	} else {
-		fmt.Println("Tu es KO.")
-	}
 	// === FIN DES AJOUTS ===
 
 	reader = bufio.NewReader(os.Stdin)
--- player/pv.go
+++ player/pv.go
@@ -22,24 +22,3 @@ func (p Pvjoueur) AfficherPV(etat EtatJeu) {
 		fmt.Println("Points de vie :", p.Pv)
 	}
 }
-
-// Fonction pour subir des dégâts
-func (p *Pvjoueur) SubirDegats(degats int) {
-	p.Pv -= degats
-	if p.Pv < 0 {
-		p.Pv = 0
-	}
-}
-
-// Fonction pour recevoir des soins
-func (p *Pvjoueur) Soigner(soin int) {
-	p.Pv += soin
-	if p.Pv > 100 {
-		p.Pv = 100
-	}
-}
-
-// Vérifie si le joueur est en vie
-func (p Pvjoueur) EstEnVie() bool {
-	return p.Pv > 0
-}
