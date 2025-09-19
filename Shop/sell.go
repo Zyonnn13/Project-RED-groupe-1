e9b77e864b713b8edff82b183532c737e99e131f
clement <clement.belmondo@ynov.com>
2025-09-19 08:42:39 +0200

    f

--- Shop/sell.go
+++ Shop/sell.go
@@ -9,7 +9,6 @@ import (
 	"Project-RED-groupe-1/monnaie"
 )
 
-// VendreObjet vend un objet de l'inventaire et ajoute les eddies
 func VendreObjet(reader *bufio.Reader, eddies *monnaie.Eddies, inventory *inventaire.Inventory, items []Item) {
 	fmt.Println("Votre inventaire :")
 	inventory.ShowInventory()
@@ -18,13 +17,11 @@ func VendreObjet(reader *bufio.Reader, eddies *monnaie.Eddies, inventory *invent
 	objet, _ := reader.ReadString('\n')
 	objet = strings.TrimSpace(objet)
 
-	// Vérifier que l'objet est dans l'inventaire
 	if !inventory.HasItem(objet) {
 		fmt.Println("Objet introuvable dans l’inventaire.")
 		return
 	}
 
-	// Déterminer le prix de vente (50% du prix d'achat)
 	prixVente := 0
 	for _, it := range items {
 		if it.Nom == objet {
@@ -38,7 +35,6 @@ func VendreObjet(reader *bufio.Reader, eddies *monnaie.Eddies, inventory *invent
 		return
 	}
 
-	// Retirer l'objet de l'inventaire et ajouter les eddies
 	inventory.RemoveItem(objet)
 	eddies.Add(prixVente)
 
